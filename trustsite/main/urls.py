8388673ac81e3f0ce7f248e5936462a7b2c10b32
Pranay Karthik <ipranayy@gmail.com>
2023-02-21 19:45:39 +0530

    Added About Us page , imported Navbar

--- trustsite/main/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path, include
-
-app_name = "main"
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("about", views.about, name="about")
-]
