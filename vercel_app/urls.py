de48b32e99fd17c20b02b912ac88cd98a70acdce
Pranay Karthik <ipranayy@gmail.com>
2023-02-23 21:56:24 +0530

    huh

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -1,4 +1,4 @@
-"""trustsite URL Configuration
+"""vercel_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for trustsite project.
+WSGI config for vercel_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trustsite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vercel_app.settings")
 
 application = get_wsgi_application()
 
